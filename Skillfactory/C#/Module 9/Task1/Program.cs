15b7b9e86fa6ea7fca5bd706a8654710ea07191d
Axentogold <Scleawe@yandex.ru>
2021-08-10 19:53:27 +0300

    Добавил задание 1 и обновил Readme.

---
+++ Skillfactory/C#/Module 9/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var exceptions = new Exception[5]
+            {
+                new UselessException(),
+                new NullReferenceException(),
+                new OutOfMemoryException(),
+                new UriFormatException(),
+                new AggregateException()
+            };
+
+            for (int i = 0; i < exceptions.Length; i++)
+                try
+                {
+                    throw exceptions[i];
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception {e.GetType()} catched on step {i}:{Environment.NewLine}{e.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine($"Exception successfully handled. Proceeding to the next step.{Environment.NewLine}");
+                }
+
+            Console.ReadKey();
+        }
+    }
+
+    class UselessException : Exception
+    {
+    }
+}
