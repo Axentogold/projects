cfda119d171bc6215ff208e9b71561ccad1643d5
Axentogold <Scleawe@yandex.ru>
2021-08-31 21:16:29 +0300

    Добавил итоговое задание.

---
+++ Skillfactory/C#/Module 11/Bot/ConsoleApp1/BotCredentials.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot
+{
+    public static class BotCredentials
+    {
+        public static readonly string BotToken = "1968970746:AAEUVus2m0HzgdNqCJIYt5TqXteUykDoyU8";
+    }
+}
