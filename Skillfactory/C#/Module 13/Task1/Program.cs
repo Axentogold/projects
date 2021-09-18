54b96645f2e32ff9cd1897db847900c915526e12
Axentogold <Scleawe@yandex.ru>
2021-09-18 13:33:23 +0300

    Добавил задание 1

---
+++ Skillfactory/C#/Module 13/Task1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Task1
+{
+    // Наша задача — сравнить производительность вставки в List<T> и LinkedList<T>.Для этого используйте уже знакомый вам StopWatch.
+
+    //На примере этого текста, выясните, какие будут различия между этими коллекциями.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // читаем весь файл в строку 
+            string text = File.ReadAllText(@"d:\Text1.txt");
+            // разбиваем в массив, используя пробел в качестве разделителя
+            var words = text.Split(" ");
+            List<string> simpleList = new List<string>();
+            LinkedList<string> linkedList = new LinkedList<string>();
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            double mean = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                timer.Restart();
+                foreach (var item in words)
+                {
+                    simpleList.Add(item);
+                }
+                timer.Stop();
+                mean += timer.Elapsed.TotalMilliseconds;
+                Console.WriteLine("Время затраченное на добавление элементов в List<T>:" + timer.Elapsed.TotalMilliseconds);
+            }
+            Console.WriteLine("\nСреднее время затраченное на добавление элементов в List<T>: " + mean / 10 + "\n");
+
+            mean = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                timer.Restart();
+                foreach (var item in words)
+                {
+                    linkedList.AddLast(item);
+                }
+                timer.Stop();
+                mean += timer.Elapsed.TotalMilliseconds;
+                Console.WriteLine("Время затраченное на добавление элементов в LinkedList<T>:" + timer.Elapsed.TotalMilliseconds);
+
+            }
+            Console.WriteLine("\nСреднее время затраченное на добавление элементов в LinkedList<T>: " + mean / 10);
+
+            Console.ReadKey();
+
+        }
+    }
+}
